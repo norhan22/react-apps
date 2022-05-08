2e7ad6220ba447973f41d53ce4097c7829a23e21
norhan <nora.moh.2011@gmail.com>
2022-05-08 18:25:03 +0200

    feat : separate the routes in new folder

--- src/index.js
+++ src/index.js
@@ -1,26 +1,10 @@
 
+import Routes from './routes/index'
 import ReactDOM from "react-dom/client";
-import {
-    BrowserRouter,
-    Routes,
-    Route,
-} from "react-router-dom";
-import App from "./layout/app";
-import HelloWorld from "./modules/HelloWorld/App";
-import TickClock from "./modules/TickClock/App";
-import Users from './modules/Users/views/index'
 
 const root = ReactDOM.createRoot(
     document.getElementById("root")
 );
 root.render(
-    <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<App />}>
-                <Route path="HelloWorld" element={<HelloWorld />} />
-                <Route path="TickClock" element={<TickClock />} />
-                <Route path="Users" element={<Users />} />
-            </Route>
-        </Routes>
-    </BrowserRouter>
+    <Routes/>
 );
